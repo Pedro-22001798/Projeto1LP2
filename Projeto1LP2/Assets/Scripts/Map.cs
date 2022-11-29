e2a27ed13cc46f3f0e96b7fb05034f09ed4d14be
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2022-11-29 17:21:22 +0000

    fixed temp

--- Projeto1LP2/Assets/Scripts/Map.cs
+++ Projeto1LP2/Assets/Scripts/Map.cs
@@ -1,20 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
 
-public class Map
-{
+// public class Map
+// {
     
-    // testing declaring and initializing 2D Array
+//     // testing declaring and initializing 2D Array
 
-    int[,] map = new int[,]
-    {
+//     int[,] map = new int[,]
+//     {
 
-        {1,2,3}, // row 0
-        {4,5,6}, // row 1
-        {7,8,9}  // row 2
+//         {1,2,3}, // row 0
+//         {4,5,6}, // row 1
+//         {7,8,9}  // row 2
 
-    }
+//     }
     
-}
+// }

80a9a83c6c588f3ef2fac5cd51886ee379fce7b2
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2022-12-03 16:48:22 +0000

    placing resources

--- Projeto1LP2/Assets/Scripts/TileFiller.cs
+++ Projeto1LP2/Assets/Scripts/TileFiller.cs
@@ -8,6 +8,8 @@ public class TileFiller : MonoBehaviour
     private GameObject mapManager;
     private NewMap mapManagerScript;
     private Tile tileSpecifications;
+    [SerializeField] private GameObject[] availableResources;
+    List<Resource> tileResources;
 
     void Start()
     {
@@ -21,12 +23,37 @@ public class TileFiller : MonoBehaviour
         {
             for(int col = 0; col < cols; col++)
             {
-                GameObject newTile = Instantiate(testObject, new Vector3(2 * col, 2 * -row, 0), Quaternion.identity);
+                GameObject newTile = Instantiate(testObject, new Vector3(col, -row, 0), Quaternion.identity);
                 TileClickInformation tileInformation = newTile.GetComponent<TileClickInformation>();
                 tileInformation.DefineCoords(row,col);
                 tileSpecifications = mapManagerScript.GetTile(row,col);
                 SpriteRenderer tileRenderer = newTile.GetComponent<SpriteRenderer>();
                 tileRenderer.color = tileSpecifications.Color;
+                tileResources = tileSpecifications.Resources;
+                for(int i = 0; i < tileResources.Count; i++)
+                {
+                    switch(tileResources[i].typeOfResource)
+                    {
+                        case ResourceType.Plants:
+                            Instantiate(availableResources[0], newTile.transform.GetChild(i).transform.position, Quaternion.identity, newTile.transform.GetChild(i));
+                            break;
+                        case ResourceType.Animals:
+                            Instantiate(availableResources[1], newTile.transform.GetChild(i).transform.position, Quaternion.identity, newTile.transform.GetChild(i));
+                            break;
+                        case ResourceType.Metals:
+                            Instantiate(availableResources[2], newTile.transform.GetChild(i).transform.position, Quaternion.identity, newTile.transform.GetChild(i));
+                            break;
+                        case ResourceType.FossilFuel:
+                            Instantiate(availableResources[3], newTile.transform.GetChild(i).transform.position, Quaternion.identity, newTile.transform.GetChild(i));
+                            break;
+                        case ResourceType.Luxury:
+                            Instantiate(availableResources[4], newTile.transform.GetChild(i).transform.position, Quaternion.identity, newTile.transform.GetChild(i));
+                            break;
+                        case ResourceType.Pollution:
+                            Instantiate(availableResources[5], newTile.transform.GetChild(i).transform.position, Quaternion.identity, newTile.transform.GetChild(i));
+                            break;
+                    }
+                }
             }
         }
     }
